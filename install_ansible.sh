58191e46b19bf23cf2a20778062f8eb8c341bf79
sanjay <sanjay.batkora@gmail.com>
2022-07-25 17:28:25 +0530

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum -y update
-sudo yum -y install epel-release
-sudo yum -y install ansible
+#sudo yum -y install epel-release
+#sudo yum -y install ansible
+sudo amazon-linux-extras install ansible2
 echo "Thank you for installing ansible with automation !"
